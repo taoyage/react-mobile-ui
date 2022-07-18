36b5c60a6a411704196e5cccc412eebd0144680b
taoyage <tougakaku@gmail.com>
2022-07-19 00:03:18 +0800

    feat: complete input component

--- packages/input/index.tsx
+++ packages/input/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import cx from 'classnames';
+
+import { CloseCircleFill } from 'antd-mobile-icons';
+
+import './styles/index.scss';
 
 export interface InputProps {
-  value: string;
+  id?: string;
+  value?: string;
   placeholder?: string;
+  /** 是否显示清除icon */
   clearable?: boolean;
+
   autoFocus?: boolean;
+  disabled?: boolean;
+  readOnly?: boolean;
+  maxLength?: number;
+  minLength?: number;
+  max?: number;
+  min?: number;
+  pattern?: string;
+  name?: string;
+  autoComplete?: 'on' | 'off';
+  autoCapitalize?: 'on' | 'off';
+  autoCorrect?: 'on' | 'off';
+  inputMode?: 'none' | 'text' | 'decimal' | 'numeric' | 'tel' | 'search' | 'email' | 'url';
+  type?: React.HTMLInputTypeAttribute;
+  onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>;
+  onKeyUp?: React.KeyboardEventHandler<HTMLInputElement>;
+  onCompositionStart?: React.CompositionEventHandler<HTMLInputElement>;
+  onCompositionEnd?: React.CompositionEventHandler<HTMLInputElement>;
+  onClick?: React.MouseEventHandler<HTMLInputElement>;
+
+  onEnterPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   onChange?: (val: string) => void;
   onClear?: () => void;
   onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void;
@@ -14,35 +42,81 @@ export interface InputProps {
 const classPrefix = `ygm-input`;
 
 const Input: React.FC<InputProps> = React.memo((props) => {
-  const [value, setValue] = React.useState<string>(props.value);
+  const [value, setValue] = React.useState<string>(props.value!);
 
-  const onChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-    props.onChange?.(e.target.value);
-  }, []);
+  const handleKeydown = React.useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (props.onEnterPress && e.code === 'Enter') {
+        props.onEnterPress(e);
+      }
+      props.onKeyDown?.(e);
+    },
+    [props]
+  );
 
-  const onFocus = React.useCallback((e: React.FocusEvent<HTMLInputElement>) => {
-    props?.onFocus(e);
-  }, []);
-
-  const onBlur = React.useCallback((e: React.FocusEvent<HTMLInputElement>) => {
-    props?.onBlur(e);
-  }, []);
+  const showClearable = React.useMemo(() => {
+    if (!props.clearable || !value || props.readOnly) return false;
+    return true;
+  }, [props.clearable, props.readOnly, value]);
 
   return (
-    <div className={classPrefix}>
+    <div className={cx(classPrefix, { [`${classPrefix}-disabled`]: props.disabled })}>
       <input
+        id={props.id}
         className={`${classPrefix}-element`}
         placeholder={props.placeholder}
         autoFocus={props.autoFocus}
         value={value}
-        onChange={onChange}
-        onFocus={onFocus}
-        onBlur={onBlur}
+        disabled={props.disabled}
+        readOnly={props.readOnly}
+        maxLength={props.maxLength}
+        minLength={props.minLength}
+        autoComplete={props.autoComplete}
+        inputMode={props.inputMode}
+        type={props.type}
+        name={props.name}
+        autoCapitalize={props.autoCapitalize}
+        autoCorrect={props.autoCorrect}
+        onKeyDown={handleKeydown}
+        onKeyUp={props.onKeyUp}
+        onCompositionStart={props.onCompositionStart}
+        onCompositionEnd={props.onCompositionEnd}
+        onClick={props.onClick}
+        onChange={(e) => {
+          setValue(e.target.value);
+          props.onChange?.(e.target.value);
+        }}
+        onFocus={(e) => {
+          props?.onFocus?.(e);
+        }}
+        onBlur={(e) => {
+          props?.onBlur?.(e);
+        }}
       />
+
+      {showClearable && (
+        <div
+          className={`${classPrefix}-clear`}
+          onMouseDown={(e) => {
+            e.preventDefault();
+          }}
+          onClick={() => {
+            setValue('');
+            props.onClear?.();
+          }}
+        >
+          <CloseCircleFill />
+        </div>
+      )}
     </div>
   );
 });
+
+Input.defaultProps = {
+  value: '',
+  id: 'ygm-input',
+  type: 'text',
+};
 
 Input.displayName = 'Input';
 
