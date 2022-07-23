3568355d9002a6ddea5531b51f3bb4cfef314d2a
taoyage <tougakaku@gmail.com>
2022-07-23 17:12:49 +0800

    feat: 完善action-sheet组件

--- packages/action-sheet/actionSheet.tsx
+++ packages/action-sheet/actionSheet.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import cx from 'classnames';
 import Popup from '@/popup';
+
+import './styles/index.scss';
 
 export interface Action {
   key: string | number;
@@ -11,13 +14,16 @@ export interface Action {
 }
 
 export interface ActionSheetProps {
-  visible?: boolean;
-  title?: React.ReactNode;
+  visible: boolean;
+  description?: React.ReactNode;
   actions: Action[];
   onClose?: () => void;
   onAction?: (action: Action, index: number) => void;
+  /** 展示取消按钮 */
+  cancelText?: React.ReactNode;
   /** 是否点击action后触发onClose回调 */
   closeOnAction?: boolean;
+  popupClassName?: string;
 }
 
 const classPrefix = `ygm-action-sheet`;
@@ -30,6 +36,8 @@ const ActionSheet: React.FC<ActionSheetProps> = React.memo((props) => {
   const renderAction = React.useCallback(
     (action: Action, index: number) => {
       const onClick = () => {
+        if (action.disabled) return;
+
         action.onClick?.();
         props.onAction?.(action, index);
 
@@ -39,27 +47,50 @@ const ActionSheet: React.FC<ActionSheetProps> = React.memo((props) => {
       };
 
       return (
-        <button key={action.key} className={`${classPrefix}-action-item`} onClick={onClick}>
+        <div
+          key={action.key}
+          className={cx(`${classPrefix}-action-item`, {
+            [`${classPrefix}-action-disabled`]: action.disabled,
+          })}
+          onClick={onClick}
+          style={{ color: action.color }}
+        >
           <div className={`${classPrefix}-action-item-name`}>{action.name}</div>
           {action.description && <div className={`${classPrefix}-action-item-desc`}>{action.description}</div>}
-        </button>
+        </div>
       );
     },
     [props.onAction, props.onClose]
   );
 
   return (
-    <Popup position="bottom" visible={props.visible!} onMaskClick={onClose}>
+    <Popup
+      position="bottom"
+      visible={props.visible!}
+      onMaskClick={onClose}
+      className={cx(`${classPrefix}-popup`, props.popupClassName)}
+    >
       <div className={classPrefix}>
-        {props.title && <div className={`${classPrefix}-title`}>{props.title}</div>}
+        {props.description && <div className={`${classPrefix}-desc`}>{props.description}</div>}
+
         <div className={`${classPrefix}-action-list`}>{props.actions.map(renderAction)}</div>
+
+        {props.cancelText && (
+          <>
+            <div className={`${classPrefix}-action-gap`} />
+            <div className={`${classPrefix}-action-item`} onClick={onClose}>
+              {props.cancelText}
+            </div>
+          </>
+        )}
       </div>
     </Popup>
   );
 });
 
 ActionSheet.defaultProps = {
-  visible: false,
+  description: '',
+  cancelText: '',
 };
 
 export default ActionSheet;
